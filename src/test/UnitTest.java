14ec411947c3841cd1e13b4909b5014a1779da1e
transformers <latifa_haibat@yahoo.com>
2023-02-22 12:04:14 -0500

    new method added and old one deleted

--- src/test/UnitTest.java
+++ src/test/UnitTest.java
@@ -3,11 +3,9 @@ package test;
 public class UnitTest {
 
 	public static void main(String[] args) {
-		int a=10;
-		int b=15;
-		int c=a+b;
-		
-		System.out.println(c);
+		 int z=13;
+		 int s=12;
+		 System.out.println(z-s);
 
 	}
 
