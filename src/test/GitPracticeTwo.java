65b817c351c4ef91d3b2fb40fe47aa5dbf5faf5b
nilofarWafi <125949938+nilofarWafi@users.noreply.github.com>
2023-02-22 17:03:19 -0500

    Update GitPracticeTwo.java
    This is for pull practice

--- src/test/GitPracticeTwo.java
+++ src/test/GitPracticeTwo.java
@@ -5,6 +5,7 @@ public class GitPracticeTwo {
 	public static void main(String[] args) {
 		String text="This is for push practice";
 		System.out.println(text);
+		System.out.println("this is for pull practice");
 
 	}
 
